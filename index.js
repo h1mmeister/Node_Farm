3fbb65eb52e642cb14b800e2f796c065129b6b78
h1m <himanshu.yadav643@gmail.com>
2020-12-15 20:38:09 +0530

    completed till routing

---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+//Files
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+// const hello = "Himanshu";
+// console.log(hello);
+
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `This is what we know about Avacado: ${textIn}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   console.log(data1);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Writing it into final.txt");
+//       });
+//     });
+//   });
+// });
+// console.log("Reading the file ->");
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//Server
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("Hello to the Website!");
+  } else if (pathName === "/product") {
+    res.end("This is the Product's page!");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(80, "127.0.0.1", () => {
+  console.log("Listening on 8000 for incoming requests");
+});
